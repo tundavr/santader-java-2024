4567a3a2e4796fb9de541883e72d614aa2b0ad35
tundavr <tundavr@gmail.com>
2024-07-10 13:57:02 -0300

    api santander dio 2024

---
+++ src/main/java/me/dio/santader_java_2024/SantaderJava2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santader_java_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderJava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderJava2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santader_java_2024/SantaderJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santader_java_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaderJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
